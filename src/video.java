e8da3a120479696ea7d042bf69b20f98f78dfb29
Joan Company <joancompanyrichart@gmail.com>
2022-12-18 12:55:55 +0100

    Commit 1

---
+++ src/video.java
@@ -0,0 +1,13 @@
+
+public class video {
+    public static void main(String[] args) {
+        name();
+
+    }
+    public static void name() {
+        for (int i = 0; i < 21; i++) {
+
+            System.out.println(i);
+        }
+    }
+}
